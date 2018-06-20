d607d258b48471054171359f9133030322694f34
朱来飞 <zhulaifei@zhulaifeideMacBook-Pro.local>
2018-06-20 11:25:53 +0800

    some code
    some code

--- BlockHooker/main.mm
+++ BlockHooker/main.mm
@@ -8,7 +8,9 @@
 
 #import <Foundation/Foundation.h>
 #include "ASBlockHooker.hpp"
-#import <objc/runtime.h>
+#include <objc/runtime.h>
+#include <objc/message.h>
+using namespace std ;
 
 //https://opensource.apple.com/source/libclosure/libclosure-67
 typedef NS_OPTIONS(int, BlockFlage) {
@@ -16,25 +18,45 @@ typedef NS_OPTIONS(int, BlockFlage) {
     BlockFlage_HAS_SIGNATURE  =    (1 << 30)
 };
 
-
-const char * signature_for_block(__main_block_impl_0 block){
-    
-    if (! (block.impl.Flags & BlockFlage_HAS_SIGNATURE)) return NULL;
-    uint8_t *desc = (uint8_t *)block.Desc;
-    desc += sizeof(struct __main_block_desc_0);
-//    if (block.impl.Flags & BlockFlage_HAS_COPY_DISPOSE) {
-//        desc += sizeof(struct IIFishBlock_descriptor_2);
-//    }
+struct block_descriptor_1 {
+    uintptr_t reserved;
+    uintptr_t size;
+};
+struct block_descriptor_2 {
+    void (*copy)(void *dst, const void *src);
+    void (*dispose)(const void *);
+};
+struct block_descriptor_3 {
+    const char *signature;
+    const char *layout;
+};
+struct block_layout {
+    void *isa;
+    volatile int32_t flags;
+    int32_t reserved;
+    void (*invoke)(void *, ...);
+    struct block_descriptor_1 *descriptor;
+};
+id temp_block_for_block(block_layout * block){
     return nil;
+}
+const char * signature_for_block(block_layout * block){
+    if (! (block->flags & BlockFlage_HAS_SIGNATURE)) return NULL;
+    uint8_t *desc = (uint8_t *)block->descriptor;
+    desc += sizeof(struct block_descriptor_1);
+    if (block->flags & BlockFlage_HAS_COPY_DISPOSE) {
+        desc += sizeof(struct block_descriptor_2);
+    }
+    struct block_descriptor_3 * des3 =  (struct block_descriptor_3 *)desc ;
+    return des3->signature;
 }
 NSMethodSignature * hooker_methodSignatureForSelector(id self,SEL _cmd ,SEL sel){
-    
-    
-    return nil;
+    block_layout *block =  (__bridge  block_layout *)self;
+    const char * signature = signature_for_block(block) ;
+    return [NSMethodSignature signatureWithObjCTypes:signature];
 }
 void hooker_forwardInvocation(id self ,SEL _cmd ,SEL sel){
-    
-    
+    block_layout *block =  (__bridge  block_layout *)self;
 }
 void method_exchange(SEL s1 ,IMP s2){
     
